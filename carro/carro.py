eed8c4e582689054e4bb33d29dceb24f0a481201
MarcosTulioM <marcos.age07@gmail.com>
2025-11-12 12:32:48 -0300

    Inicio configurando docker, carro e isccp

---
+++ carro/carro.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+import time
+import random
+import json
+import os
+
+client = mqtt.Client()
+client.connect("mqtt_broker", 1883, 60)  #Container do Mosquitto no docker
+
+carro_id = os.environ.get("CAR_ID", "0")
+print(f"[carro {carro_id}] Iniciando publicador...", flush=True)
+
+def gerar_dados_pneu():
+    return {
+        "pressao": round(random.uniform(18.0, 22.0), 2),
+        "temperatura": round(random.uniform(70.0, 100.0), 2)
+    }
+
+while True:
+    dados = gerar_dados_pneu()
+    topico = f"carro/{carro_id}/pneu"
+    client.publish(topico, json.dumps(dados))
+    print(f"[carro {carro_id}] Publicado: {topico} -> {dados}")
+    time.sleep(5)
---
+++ isccp/isccp.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+import json
+import os
+
+name = os.environ.get("NAME", "ISCCP")
+
+print(f"[{name}] Iniciando assinante...", flush=True)
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("carro/+/pneu")  # escuta todos os carros
+
+def on_message(client, userdata, msg):
+    dados = json.loads(msg.payload.decode())
+    print(f"Recebido de {msg.topic}: {dados}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt_broker", 1883, 60)
+client.loop_forever()
